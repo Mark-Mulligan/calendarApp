6e3e0be1d5b840c3d3d9dce3cec0c94507ade827
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2020-10-28 00:27:26 -0500

    initial commit

---
+++ Develop/script.js
@@ -0,0 +1,46 @@
+let timeblockContainer = document.querySelector('.timeblock-container');
+
+let tasks = ['', '', '', '', '', '', '', '', ''];
+
+tasks = JSON.parse(localStorage.getItem("taskList"));
+console.log(tasks);
+
+$('.description').each(function(index) {
+    $(this).text(tasks[index]);
+})
+
+$('.saveBtn').click(function (e) {
+    event.preventDefault();
+    let timeBlock = $(this).attr('data-time-block');
+    let task = $.trim($(`#${timeBlock}Tasks`).val());
+    let index = convertTimeToIndex(timeBlock);
+    tasks[index] = task;
+    console.log(tasks);
+
+    localStorage.setItem("taskList", JSON.stringify(tasks));
+})
+
+function convertTimeToIndex(input) {
+    switch (input) {
+        case '9am':
+            return 0;
+        case '10am':
+            return 1;
+        case '11am':
+            return 2;
+        case '12pm':
+            return 3;
+        case '1pm':
+            return 4;
+        case '2pm':
+            return 5;
+        case '3pmam':
+            return 6;
+        case '4pm':
+            return 7;
+        case '5pm':
+            return 8;
+        default:
+            return 0;
+    }
+}
